cf868b4303d661149706f3c9fa5ed476619c46f4
CoderSid063 <sudhansupanda063@gmail.in>
2023-11-04 11:43:10 +0530

    Move addToCart() to cart.js

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,1 +1,20 @@
 export const cart = [];
+
+export function addToCart(productId){
+   let matchItem;
+     cart.forEach((cartItem) => {
+       if(productId === cartItem.productId){
+         matchItem = cartItem;
+       }
+     });
+ 
+     if(matchItem){
+       matchItem.quantity  += 1;
+     }
+     else{
+       cart.push({
+         productId : productId ,
+         quantity : 1,
+       });
+     }
+ }
